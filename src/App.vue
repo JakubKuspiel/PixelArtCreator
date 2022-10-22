5290c28049c6d8df9e6b92c6764e3ae0c0c4a14d
KubaQ89 <jakub.kuspiel@web.de>
2022-10-22 18:52:51 +0200

    reverse button design

--- src/App.vue
+++ src/App.vue
@@ -109,17 +109,5 @@ function printCanvas(){
   width: 40rem;
 }
 
-.button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-}
-.button:hover {
-  cursor: pointer;
-}
+
 </style>
